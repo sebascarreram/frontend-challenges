4f7ab336a84d86bbd79a90251c97f2fdbc95950f
Sebastian Carrera <sebascarreram@hotmail.com>
2021-05-08 20:20:55 -0500

    added a file for projects list

---
+++ projects.js
@@ -0,0 +1,40 @@
+const url = "https://github.com/sebascarreram/frontend-challenges/tree/master";
+
+export const projects = [
+  {
+    name: "four-card-feature-section-master",
+    github_url: `${url}/four-card-feature-section-master`,
+  },
+  {
+    name: "base-apparel-coming-soon-master",
+    github_url: `${url}/base-apparel-coming-soon-master`,
+  },
+  {
+    name: "signup-form-master",
+    github_url: `${url}/signup-form-master`,
+  },
+  {
+    name: "single-price-grid-component-master",
+    github_url: `${url}/single-price-grid-component-master`,
+  },
+  {
+    name: "ping-coming-soon-page-master",
+    github_url: `${url}/ping-coming-soon-page-master`,
+  },
+  {
+    name: "huddle-landing-page",
+    github_url: `${url}/huddle-landing-page`,
+  },
+  {
+    name: "huddle-landing-page-with-single",
+    github_url: `${url}/huddle-landing-page-with-single`,
+  },
+  {
+    name: "fylo-landing-page",
+    github_url: `${url}/fylo-landing-page`,
+  },
+  {
+    name: "stats-preview-card-component-main",
+    github_url: `${url}/stats-preview-card-component-main`,
+  },
+];
