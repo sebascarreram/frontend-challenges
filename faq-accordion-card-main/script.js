45931c3e671681bdfa7154a23e7421012522ea10
Sebastian Carrera <sebascarreram@hotmail.com>
2021-05-15 16:10:11 -0500

    Modified collapsible for list

--- faq-accordion-card-main/script.js
+++ faq-accordion-card-main/script.js
@@ -1,1 +1,21 @@
-console.log("adas")
+let acc = document.getElementsByClassName("titleSub");
+let i;
+//console.log(acc);
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    /* 
+     Toggle between adding and removing the "active" class,
+     to highlight the button that controls the panel
+      */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    //console.log(this.nextElementSibling);
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
