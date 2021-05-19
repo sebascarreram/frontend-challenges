d74f81ebc960018cfd4e563af152a52076a5ecde
Sebastian Carrera <sebascarreram@hotmail.com>
2021-05-18 19:07:38 -0500

    add socials when user clicked in shared

---
+++ article-preview-component-master/script.js
@@ -0,0 +1,4 @@
+const btnShare = document.querySelector("#btnShare");
+const social = document.querySelector(".social");
+
+btnShare.addEventListener("click", () => social.classList.toggle("active"));
