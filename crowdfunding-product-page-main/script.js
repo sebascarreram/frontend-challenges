41fd6bf72c687fba7e173d6bafc27daa061ab4eb
Sebastian Carrera <sebascarreram@hotmail.com>
2021-06-02 21:38:13 -0500

    Modify bookmark with toggle

--- crowdfunding-product-page-main/script.js
+++ crowdfunding-product-page-main/script.js
@@ -10,14 +10,31 @@ const body = document.querySelector("body");
 // Get the button that opens the modal
 const btn = document.getElementById("btn__hamburger");
 
+// Get bookmark
+//const bookmarkBtn = document.getElementById("btnBookmark");
+const bookmarkBtn = document.querySelector(".btnBookmark--icon");
+
+// Get bookmark text
+const bookmarkText = document.querySelector(".btnBookmark--text");
+
 // When the user clicks the button
 btn.onclick = function () {
-	// Show modal
+  // Show modal
   modal.classList.toggle("nav--active");
-	// Show background dark
+  // Show background dark
   backgroundDark.classList.toggle("background-modal--active");
-	// Change hamburger to close
+  // Change hamburger to close
   btn.classList.toggle("header__hamburger--active");
-	// Active overflow: Hidden
+  // Active overflow: Hidden
   body.classList.toggle("body--active");
 };
+
+bookmarkBtn.addEventListener("click", (e) => {
+  bookmarkBtn.classList.toggle("btnBookmark--icon--active");
+  bookmarkText.classList.toggle("btnBookmark--active");
+});
+
+bookmarkText.addEventListener("click", () => {
+  bookmarkBtn.classList.toggle("btnBookmark--icon--active");
+  bookmarkText.classList.toggle("btnBookmark--text--active");
+});
